1375a0a9098fbfdeef9609b3510d8e0d82e3ccc0
Lucas Tran <lucastra@stud.ntnu.no>
2023-02-09 12:12:59 +0100

    Create UI for Ads
    Start working on UI for ads
    (#7)
    @lucastra

--- skurApp/src/App.js
+++ skurApp/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import CreateAd from './pages/CreateAds/createAd'
 
 function App() {
   return (
+    <>
     <div className="App">
       <h1>Hello world</h1>
     </div>
+    <CreateAd />
+    </>
   );
 }
 
