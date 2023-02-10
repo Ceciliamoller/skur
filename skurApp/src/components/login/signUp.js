c5c5ec171cd940d6a320e21b875202c0ce100e2b
Magnus <magnus@trandokken.com>
2023-02-10 12:02:18 +0100

    Method for signup (#5) @magnutra

--- skurApp/src/components/login/signUp.js
+++ skurApp/src/components/login/signUp.js
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import './login.css';
+import { useNavigate } from 'react-router-dom'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../config/firebaseConfig';
 
 
-function SignUp(){
+function SignUp() {
 
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const onSignup = (e) => {
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                navigate("/")
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // TODO: add responsetext to user
+                console.log(errorCode, errorMessage)
+            });
+
+    }
 
     return (
         <div className="signUpPage">
-            <div className="container">
-                <h2>Eller lag en bruker:</h2>
-                <input type="text" placeholder="Navn" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Passord" />
-                <button className="sighUpBtn">Lag bruker</button>
-            </div>
+            <form onSubmit={onSignup}>
+                <div className="container">
+                    <h2>Eller lag en bruker:</h2>
+                    <input type="text" placeholder="Navn" />
+                    <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" placeholder="Passord" onChange={(e) => setPassword(e.target.value)} />
+                    <button type="submit" className="sighUpBtn">Lag bruker</button>
+                </div>
+            </form>
         </div>
     );
 }
