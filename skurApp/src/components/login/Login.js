75cf240a179a4cb32ac5a92be5b2dcd1bd1d0fab
Magnus Trandokken <magnutra@stud.ntnu.no>
2023-02-10 11:30:02 +0100

    User sign in methos

--- skurApp/src/components/login/Login.js
+++ skurApp/src/components/login/Login.js
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import './login.css';
+import { useNavigate } from 'react-router-dom'
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../config/firebaseConfig';
 
-function Login(){
+function Login() {
 
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const onLogin = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                navigate("/")
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // TODO: add responsetext to user
+                console.log(errorCode, errorMessage)
+            });
+
+    }
 
     //Skal legges inn onclick={} is alle buttons (som gir popups kanksje??)
     return (
         <div className="loginPage">
-            <div className="container" >
-                <div className="loginContainer">
-                    <h2>Logg inn:</h2>
-                    <button className="loginWithGoogleBtn">Logg inn med Google</button>
-                    <h2>Eller logg inn med email</h2>
-                    <input type="email" placeholder="Email" />
-			        <input type="password" placeholder="Password" />
-                    <button className="loginWithEmailBtn">Logg inn</button>
+            <form onSubmit={onLogin}>
+                <div className="container" >
+                    <div className="loginContainer">
+                        <h2>Logg inn:</h2>
+                        <button className="loginWithGoogleBtn">Logg inn med Google</button>
+                        <h2>Eller logg inn med email</h2>
+                        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                        <button type="submit" id="loginWithEmailBtn" >Logg inn</button>
+                    </div>
                 </div>
-                <div className="signUpContainer">
-                    <h2>Eller lag en bruker:</h2>
-                    <input type="text" placeholder="Navn" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Passord" />
-                    <button className="sighUpBtn">Lag bruker</button>
-
-                </div>
-                
-            </div>
-
-
-            
+            </form>
         </div>
     );
 }
