6dfdcd928900da41a0dde43e8395efd98cb845cb
Magnus <magnus@trandokken.com>
2023-02-14 14:19:19 +0100

    Add firestore Add method to push new tool to firestore (#8) @magnutra

--- skurApp/src/components/CreateTools/createTool.js
+++ skurApp/src/components/CreateTools/createTool.js
@@ -1,10 +1,12 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import "../../App.js"
 import "./createTool.css"
 import { Box, Text, VStack, Button, Select } from '@chakra-ui/react'
 import { Input } from '@chakra-ui/react'
 import { ChakraProvider } from '@chakra-ui/react'
+import { collection, addDoc } from "firebase/firestore";
+import { firestoreService } from '../../config/firebaseConfig';
 
 
 export default function createTools() {
@@ -13,53 +15,62 @@ export default function createTools() {
     const [newTitle, setNewTitle] = useState("");
     const [newDescription, setNewDescription] = useState("");
     const [newPrice, setNewPrice] = useState("");
-    const [newTool, setNewTool] = useState("");
+    const [toolCategory, setNewToolCategory] = useState("");
 
-    function handleCreateTool() {
-        
+    /* Method for creating new tool in firebase */
+    async function handleCreateTool() {
+
+        try {
+            const docRef = await addDoc(collection(firestoreService, "tools"), {
+                toolName: newTitle,
+                price: newPrice,
+                description: newDescription,
+                category: toolCategory
+            });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+
     }
 
-    return(
-<ChakraProvider>
-      <Box alignItems="center">
+    return (
+        <ChakraProvider>
+            <Box alignItems="center">
 
-        <VStack mt="50px" spacing="50px" mb="50px">
-            <Text fontSize="40px"  mb="-20px"> Opprett en annonse</Text>
-            <Box>
-                <Text  fontSize="large" textColor="black"> Tittel </Text>
-                <Input id="titleID" placeholder='Skriv inn tittel...' w="500px" focusBorderColor='567189' onChange={ (event) => setNewTitle(event.target.value)}/>
-                <Text> {newTitle} </Text>
+                <VStack mt="50px" spacing="50px" mb="50px">
+                    <Text fontSize="40px" mb="-20px"> Opprett en annonse</Text>
+                    <Box>
+                        <Text fontSize="large" textColor="black"> Tittel </Text>
+                        <Input id="titleID" placeholder='Skriv inn tittel...' w="500px" focusBorderColor='567189' onChange={(event) => setNewTitle(event.target.value)} />
+                    </Box>
+                    <Box>
+                        <Text fontSize="large" textColor="black" > Beskrivelse </Text>
+                        <Input id="descriptionID" placeholder='Skriv inn en beskrivelse...' w="500px" h="200px" focusBorderColor='567189' onChange={(event) => setNewDescription(event.target.value)} />
+                    </Box>
+                    <Box>
+                        <Text fontSize="large" textColor="black" > Pris </Text>
+                        <Input type="number" id="priceID" placeholder='Skriv inn en pris...' w="500px" focusBorderColor='#567189' onChange={(event) => setNewPrice(event.target.value)} />
+                    </Box>
+                    <Box>
+                        <Text fontSize="large" textColor="black" > Kategori </Text>
+                        <Select width="500px" placeholder="Velg kategori" onChange={(event) => setNewToolCategory(event.target.value)}>
+                            <option value="Hammer">
+                                Hammer
+                            </option>
+                            <option value="Skrutrekker">
+                                Skrutrekker
+                            </option>
+                            <option value="Sag">
+                                Sag
+                            </option>
+                        </Select>
+                    </Box>
+                    <Button colorScheme='blue' onClick={handleCreateTool}> Opprett annonse
+                    </Button>
+                </VStack>
             </Box>
-            <Box>
-                <Text  fontSize="large" textColor="black" > Beskrivelse </Text>
-                <Input id="descriptionID"  placeholder='Skriv inn en beskrivelse...' w="500px" h="200px" focusBorderColor='567189' onChange={ (event) => setNewDescription(event.target.value)}/>
-                <Text> {newDescription} </Text>
-            </Box>
-            <Box>
-                <Text  fontSize="large" textColor="black" > Pris </Text>
-                <Input id="priceID"  placeholder='Skriv inn en pris...' w="500px" focusBorderColor='#567189' onChange={ (event) => setNewPrice(event.target.value)}/>
-                <Text> {newPrice} </Text>
-            </Box>
-            <Box>
-                <Text  fontSize="large" textColor="black" > Kategori </Text>
-                <Select width="500px" placeholder="Velg kategori" onChange={(event) => setNewTool(event.target.value)}>
-                    <option value="Hammer">
-                        Hammer
-                    </option>
-                    <option value="Skrutrekker">
-                        Skrutrekker
-                    </option>
-                    <option value="Sag">
-                        Sag
-                    </option>
-                </Select>
-                <text> Tool: {newTool} </text>
-            </Box>
-            <Button colorScheme='blue' onClick={handleCreateTool}> Opprett annonse
-            </Button>
-        </VStack>
-        </Box>
-</ChakraProvider>
+        </ChakraProvider>
 
     )
 }
